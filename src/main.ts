e31e59097f2feda93ac37b5b44b0000079ee4dae
phamvlap <phamvlap302@gmail.com>
2025-02-06 15:02:39 +0700

    feat: add helmet and enable cors

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import helmet from 'helmet';
 import { VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
@@ -5,6 +6,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.use(helmet());
+  app.enableCors();
   app.enableVersioning({
     type: VersioningType.URI,
     prefix: 'v',
